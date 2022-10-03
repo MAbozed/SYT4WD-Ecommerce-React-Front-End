fdb99033086f9447406664b1bda01958f36ad053
MAbozed <mabozed.com@gmail.com>
2022-10-03 19:55:54 +0300

    Initialize admin Dashboard files

---
+++ react-app/src/layout/admin/MasterLayout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const MasterLayout = () => {
+    return(
+        <div className='sb-nav-fixed'>
+            <Navbar />
+        </div>
+    )
+        
+    
+}
+export default MasterLayout;
