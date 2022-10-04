6b95be5ead9dc07222cb73b9b9ae07fd350746a7
MAbozed <mabozed.com@gmail.com>
2022-10-04 18:04:41 +0300

    create Login & Register Pages

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,12 +6,16 @@ import MasterLayout from './layout/admin/MasterLayout';
 import Dashboard from './components/admin/Dashboard';
 import Profile from './components/admin/Profile';
 import Home from './layout/frontend/Home';
+import Login from './components/frontend/auth/Login';
+import Register from './components/frontend/auth/Register';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='register' element={<Register />}/>
             <Route  path='/admin'  element={<MasterLayout/>}/>
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/profile" element={<Profile />} />
